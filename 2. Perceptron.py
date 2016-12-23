c9a64bec70fbd3f1415ab408da1bab6bbd580f5f
Minzhe Zhang <zhang316421071@163.com>
2016-12-23 04:34:25 -0600

    Perceptron

--- 2. Perceptron.py
+++ 2. Perceptron.py
@@ -1,3 +1,12 @@
+### 2. Perceptron 
+#########################################
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
 class Perceptron(object):
     '''
     Perceptron classifier
@@ -17,7 +26,7 @@ class Perceptron(object):
     Number of misclassifications in every epoch.
     '''
     
-    def _init_(self, eta = 0.01, n_iter = 10):
+    def __init__(self, eta = 0.01, n_iter = 10):
         self.eta = eta
         self.n_iter = n_iter
     
@@ -28,9 +37,7 @@ class Perceptron(object):
         Parameters
         ----------
         X : {array-like}, shape = [n_samples, n_features]
-        Training vectors, where n_samples
-        is the number of samples and
-        n_features is the number of features.
+        Training vectors, where n_samples is the number of samples and n_features is the number of features.
         
         y : array-like, shape = [n_samples]
         Target values.
@@ -41,3 +48,79 @@ class Perceptron(object):
         '''
         self.w_ = np.zeros(1 + X.shape[1])
         self.errors_ = []
+        
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+        
+    def net_input(self, X):
+        '''
+        calculate net input
+        '''
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, X):
+        '''
+        Return class label after unit step
+        '''
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'grey', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    # plot class sample
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+
+# read data
+# ----------------------
+df = pd.read_csv('2.iris.data.txt', header=None)           
+df.tail()
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc='upper left')
+plt.show()
+
+# strat training
+# ---------------
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassification')
+plt.show()
+
+# visualization
+# ----------------
+plot_decision_regions(X, y, classifier=ppn)
