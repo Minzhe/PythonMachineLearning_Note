a311987c0d3bdc59008169225777d7feaeef3de0
Minzhe Zhang <zhang316421071@163.com>
2017-01-08 17:00:39 -0600

    6. model evaluation

---
+++ 6.1. crossValidation.py
@@ -0,0 +1,50 @@
+###   Model evaluation
+#################################
+
+# ------ read and prepare data ------ #
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/python-machine-learning-book/master/code/datasets/wdbc/wdbc.data', header=None)
+
+from sklearn.preprocessing import LabelEncoder
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+le.transform(['M', 'B'])
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# --- Combining transformers and estimators in a pipeline --- #
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+pipe_lr = Pipeline([('scl', StandardScaler()),
+                    ('pca', PCA(n_components=2)),
+                    ('clf', LogisticRegression(random_state=1))])
+
+pipe_lr.fit(X_train, y_train)
+print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+y_pred = pipe_lr.predict(X_test)
+
+# ----- k-flod cross-validation ----- #
+import numpy as np
+from sklearn.cross_validation import StratifiedKFold
+
+kfold = StratifiedKFold(y=y_train, n_folds=10, random_state=1)
+scores = []
+for k, (train, test) in enumerate(kfold):
+    pipe_lr.fit(X_train[train], y_train[train])
+    score = pipe_lr.score(X_train[test], y_train[test])
+    scores.append(score)
+    print('Fold: %s, Class dist.: %s, Accuracy: %.3f' % (k+1, np.bincount(y_train[train]), score))
+
+print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+from sklearn.cross_validation import cross_val_score
+
+scores = cross_val_score(estimator=pipe_lr, X=X_train, y=y_train, cv=10, n_jobs=1)
+print('CV accuracy scores: %s' % scores)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
---
+++ 6.2. parameter tunning.py
@@ -0,0 +1,158 @@
+###     parameters
+###############################
+
+# ------- learning curve -------- #
+import matplotlib.pyplot as plt
+
+if Version(sklearn_version) < '0.18':
+    from sklearn.learning_curve import learning_curve
+else:
+    from sklearn.model_selection import learning_curve
+
+pipe_lr = Pipeline([('scl', StandardScaler()),
+                    ('clf', LogisticRegression(penalty='l2', random_state=0))])
+
+train_sizes, train_scores, test_scores =\
+                learning_curve(estimator=pipe_lr,
+                               X=X_train,
+                               y=y_train,
+                               train_sizes=np.linspace(0.1, 1.0, 10),
+                               cv=10,
+                               n_jobs=1)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(train_sizes, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='training accuracy')
+
+plt.fill_between(train_sizes,
+                 train_mean + train_std,
+                 train_mean - train_std,
+                 alpha=0.15, color='blue')
+
+plt.plot(train_sizes, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+
+plt.fill_between(train_sizes,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xlabel('Number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.0])
+plt.tight_layout()
+# plt.savefig('./figures/learning_curve.png', dpi=300)
+plt.show()
+
+# -------- validation curve --------- #
+if Version(sklearn_version) < '0.18':
+    from sklearn.learning_curve import validation_curve
+else:
+    from sklearn.model_selection import validation_curve
+
+
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+                estimator=pipe_lr, 
+                X=X_train, 
+                y=y_train, 
+                param_name='clf__C', 
+                param_range=param_range,
+                cv=10)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean, 
+         color='blue', marker='o', 
+         markersize=5, label='training accuracy')
+
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+
+plt.plot(param_range, test_mean, 
+         color='green', linestyle='--', 
+         marker='s', markersize=5, 
+         label='validation accuracy')
+
+plt.fill_between(param_range, 
+                 test_mean + test_std,
+                 test_mean - test_std, 
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.tight_layout()
+# plt.savefig('./figures/validation_curve.png', dpi=300)
+plt.show()
+
+# ------ hypeterparameters tunning --------- #
+from sklearn.svm import SVC
+if Version(sklearn_version) < '0.18':
+    from sklearn.grid_search import GridSearchCV
+else:
+    from sklearn.model_selection import GridSearchCV
+
+pipe_svc = Pipeline([('scl', StandardScaler()),
+            ('clf', SVC(random_state=1))])
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+
+param_grid = [{'clf__C': param_range, 
+               'clf__kernel': ['linear']},
+                 {'clf__C': param_range, 
+                  'clf__gamma': param_range, 
+                  'clf__kernel': ['rbf']}]
+
+gs = GridSearchCV(estimator=pipe_svc, 
+                  param_grid=param_grid, 
+                  scoring='accuracy', 
+                  cv=10,
+                  n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print('Test accuracy: %.3f' % clf.score(X_test, y_test))
+
+# ---------- model selection ------------ #
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring='accuracy',
+                  cv=2)
+
+# Note: Optionally, you could use cv=2 
+# in the GridSearchCV above to produce
+# the 5 x 2 nested CV that is shown in the figure.
+
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+from sklearn.tree import DecisionTreeClassifier
+
+gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0),
+                  param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}],
+                  scoring='accuracy',
+                  cv=2)
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
